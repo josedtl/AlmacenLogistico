5994bc1f7b4be6037a01258a5108919f69f572b5
David Timo <josedtl115@gmail.com>
2023-03-23 22:11:26 -0500

    Cambio TOTAL

--- server/BusinessLayer/Horario_Business.py
+++ server/BusinessLayer/Horario_Business.py
@@ -2,9 +2,18 @@ from DataLayer.Producto_Data import *
 from EntityLayer.Catalogo.ProductoEntity import *
 
 class Producto_Business:
-    def SaveHorario(Ent: ProductoEntity):
+    def Get_PersonaNaturalItems():
         try:
-            data = Producto_Data.SaveHorario(Ent)
-            return data
+            data = Producto_Data.Get_PersonaNaturalItems()
+            print(data)
+            jsonData = []
+
+            for row in data:
+                jsonStr = json.dumps(row.__dict__)
+                jsonStr = json.loads(jsonStr)
+                
+                jsonData.append(jsonStr)
+
+            return jsonData
         except Exception as e:
             print(e)
