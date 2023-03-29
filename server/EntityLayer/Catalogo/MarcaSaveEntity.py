c8af703e6a989041d5f73f29f59dbc207bfa2bff
David Timo <josedtl115@gmail.com>
2023-03-29 08:40:51 -0500

    Cambio TOTAL

--- server/EntityLayer/Catalogo/MarcaSaveEntity.py
+++ server/EntityLayer/Catalogo/MarcaSaveEntity.py
@@ -1,9 +1,9 @@
 import datetime
 from pydantic import BaseModel
 
-class MarcaSaveEntity(BaseModel):
-    MarcaId: int
-    Nombre :str
-    FechaRegistro:  datetime
-    CodUsuario : str
-    Estado: bool
+# class MarcaSaveEntity(BaseModel):
+#     MarcaId: int
+#     Nombre :str
+#     FechaRegistro:  datetime
+#     CodUsuario : str
+#     Estado: bool
