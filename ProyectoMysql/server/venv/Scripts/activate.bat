62e403f429c076f82cae25312f6a73338394f1dc
josedtl <30784219+josedtl@users.noreply.github.com>
2024-02-07 10:32:26 -0500

    ca

--- ProyectoMysql/server/venv/Scripts/activate.bat
+++ ProyectoMysql/server/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\DAVID\Documents\GitHub\AlmacenLogistico\Proyecto\server\venv
+set VIRTUAL_ENV=C:\Users\DAVID\Documents\GitHub\AlmacenLogistico\ProyectoMysql\server\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
