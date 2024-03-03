215b061a78e38ea14bfb41608b2bdb96090d2e3f
josedtl <30784219+josedtl@users.noreply.github.com>
2024-03-02 23:04:15 -0500

    Vers

--- ProyectoORM/server/src/app.module.ts
+++ ProyectoORM/server/src/app.module.ts
@@ -3,13 +3,11 @@ import { CategoriaModule } from './Modules/Categoria.module';
 import { TipoProductoModule } from './Modules/TipoProducto.module';
 import { MarcaModule } from './Modules/Marca.module';
 import { ModeloModule } from './Modules/Modelo.module';
-import { PersonaModule } from './Modules/Entidad.module';
 import { ListaRelacionModule } from './Modules/ListaRelacion.module';
 import { ProductoModule } from './Modules/Producto.module';
 import { GeneralModule } from './Modules/General.module';
-import { EntidadEmpresa } from './Modules/EntidadEmpresa.module';
 @Module({
-  imports: [CategoriaModule, PersonaModule, ListaRelacionModule, TipoProductoModule, MarcaModule, ModeloModule, ProductoModule,GeneralModule,EntidadEmpresa],
+  imports: [CategoriaModule, ListaRelacionModule, TipoProductoModule, MarcaModule, ModeloModule, ProductoModule,GeneralModule],
   controllers: [],
   providers: [],
 })
