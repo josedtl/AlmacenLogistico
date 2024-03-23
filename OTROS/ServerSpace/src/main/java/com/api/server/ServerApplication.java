252ed85d799815b0d21c1216ff75c8568d7c60ed
josedtl <30784219+josedtl@users.noreply.github.com>
2024-03-23 00:15:08 -0500

    v

--- OTROS/ServerSpace/src/main/java/com/api/server/ServerApplication.java
+++ OTROS/ServerSpace/src/main/java/com/api/server/ServerApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
+
 //@EnableSwagger2
 public class ServerApplication {
 
