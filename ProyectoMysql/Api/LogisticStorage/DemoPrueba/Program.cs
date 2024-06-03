7aede29a6e107ea33b28e2e15bfab952a91011f2
josedtl <30784219+josedtl@users.noreply.github.com>
2024-06-03 01:30:38 -0500

    Version 1.2
    Add

---
+++ ProyectoMysql/Api/LogisticStorage/DemoPrueba/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoPrueba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Monto = 100;
+            double Igv = 1.18;
+            double Total = Monto / Igv;
+
+            // Redondear a dos decimales
+            double roundedToTwoDecimals = Math.Round(Total, 2);
+            Console.WriteLine("Redondeado a dos decimales: " + roundedToTwoDecimals);
+
+            // Redondear al número entero más cercano
+            int roundedToNearestInteger = (int)Math.Round(Total);
+            Console.WriteLine("Redondeado al número entero más cercano: " + roundedToNearestInteger);
+        }
+    }
+}
