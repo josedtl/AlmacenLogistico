884c2aaf027450238d8a59c00eafe27533e05172
timoruth02 <134884874+timoruth02@users.noreply.github.com>
2024-08-06 01:19:46 -0500

    v 1.7
    cargado reserva en el detalle

--- ProyectoMysql/client/src/Components/Despacho/DespachoDetalle/Save.tsx
+++ ProyectoMysql/client/src/Components/Despacho/DespachoDetalle/Save.tsx
@@ -34,7 +34,6 @@ function Page() {
     //const [items, setItems] = useState<DespachoDetalleEntity[]>([]);
 
     const [items, setItems] = useState<DespachoDetalleModel[]>([]);
-    //const [Ent, setEnt] = useState<DespachoDetalleEntity>(new DespachoDetalleEntity());
 
 
     const [CargarPage, setCargarPage] = React.useState(true);
@@ -83,10 +82,7 @@ function Page() {
         if (Id > 0) {
 
             const Resp_Cabecera = await sDespacho.GetItemCabecera(Id);
-            Resp_Cabecera[0].Action = ProcessActionEnum.Update
-
             setEnt(Resp_Cabecera[0]);
-
             const Resp_Detalle = await sDespacho.GetItemDetalle(Id);
 
             if (Resp_Detalle.length > 0) {
@@ -94,29 +90,28 @@ function Page() {
                 Resp_Detalle.map((data) => {
                     data.Action = ProcessActionEnum.Update;
 
-
                 })
 
                 setItems(Resp_Detalle);
                 Ent.DetalleItems = Resp_Detalle
             }
-        }
 
-        if (Id > 0) {
-            const Resp_Detalle = await sDespacho.GetItemDetalle(Id);
-            Resp_Detalle[0].Action = ProcessActionEnum.Update
 
+  // const filterItems = items.filter(fdata =>
+  //   fdata.Codigo.toLowerCase().includes(Busqueda.toLowerCase())
+  // );
 
             const Resp_DetalleItem = await sDespacho.GetDetalleOP(Id);
 
-            if (Resp_DetalleItem.length > 0) {
-                Resp_DetalleItem.map((data) => {
-                    data.Action = ProcessActionEnum.Update;
 
-                })
-
-            }
+            Ent.DetalleItems.map((detalle) => {
+                    detalle.DetalleReserva=Resp_DetalleItem.filter(d =>d.OrdenPedidoDetalleId = detalle.OrdenPedidoDetalleId)
+            })
         }
+
+
+
+        console.log(Ent);
 
         setCargarPage(false);
     }
@@ -126,33 +121,27 @@ function Page() {
     const AddProducto = async () => {
         try {
 
-            Ent.Action = Ent.DespachoId == 0 ? 1 : 3;
-            if (Ent.DespachoId === 0) {
-                Ent.FechaRegistro = new Date();
-                Ent.Action = 1;
-            }
+          
+            // Ent.DetalleItems = items;
 
-            Ent.DetalleItems = items;
-
-            // console.log(Ent);
+   
             const savedItem = await sDespacho.saveItem(Ent);
-            console.log('NADA')
-            console.log(savedItem);
+       
 
-            if (savedItem) {
+            // if (savedItem) {
 
-                setEnt(savedItem)
-                setItems(savedItem.DetalleItems)
-                getCargarDatos(savedItem.DespachoId);
-                messageAdd.open({
-                    type: 'success',
-                    content: 'Se guardó correctamente.',
-                });
+            //     setEnt(savedItem)
+            //     setItems(savedItem.DetalleItems)
+            //     getCargarDatos(savedItem.DespachoId);
+            //     messageAdd.open({
+            //         type: 'success',
+            //         content: 'Se guardó correctamente.',
+            //     });
 
 
-            } else {
+            // } else {
 
-            }
+            // }
 
         }
         catch (e) {
@@ -168,15 +157,13 @@ function Page() {
         e.preventDefault();
         KeyTabs;
 
-        if (Ent.EntidadEntregadoId === 0) {
+        if (Ent.EntidadId === 0) {
             setValEntregado('error');
             messageAdd.open({ type: 'error', content: 'Seleccione al responsable', });
             return;
         }
 
         selectedEntregado
-        console.log(Ent.Action);
-        console.log(Ent);
         modal.confirm({
             title: 'Mensaje del Sistema',
             icon: <ExclamationCircleOutlined />,
@@ -326,8 +313,8 @@ function Page() {
                                             defaultActiveFirstOption={false}
                                             filterOption={false}
                                             onSearch={search_Persona}
-                                            value={Ent.EntidadEntregadoId === 0 ? null : Ent.EntidadEntregadoId}
-                                            key={Ent.EntidadEntregadoId}
+                                            value={Ent.EntidadId === 0 ? null : Ent.EntidadId}
+                                            key={Ent.EntidadId}
                                             onChange={onchange_Persona}
                                         >
                                             {optionsEntregado.map((Persona) => (
--- ProyectoMysql/client/src/Models/DespachoEntity.tsx
+++ ProyectoMysql/client/src/Models/DespachoEntity.tsx
@@ -3,38 +3,19 @@ import { ProcessActionEnum } from '../Lib/ResourceModel/Enum'
 import { Reserva } from '../Models/ReservaEntity'
 
 export class DespachoEntity {
-    OrdenPedidoId: number;
-    Codigo: string;
-    Nombre: string;
-    NombreCompleto: string;
-    Documento: string;
-    FechaRegistro: Date;
-    Action: ProcessActionEnum
-    DespachoId: 0;
-    EntidadEntregadoId: 0;
-    FechaHoraEntrega: Date;
-    ProcesoId: number;
-    NomProceso: string;
-    EntidadId: number;
-    NomResponsable: string;
+
+    OrdenPedidoId: number
+    NomProceso: string
+    ProcesoId: number
+    EntidadId: number 
+    NomResponsable: string
     DetalleItems: DespachoDetalleModel[]
     constructor() {
-        this.OrdenPedidoId = 0;
-        this.Codigo = '';
-        this.Nombre = '';
-        this.NombreCompleto = '';
-        this.Documento = '';
-        this.FechaRegistro = new Date();
-        this.Action = ProcessActionEnum.Add;
-        this.DespachoId = 0;
-        this.EntidadEntregadoId = 0;
-        this.FechaHoraEntrega = new Date();
         this.OrdenPedidoId = 0;
         this.ProcesoId = 0;
         this.NomProceso = "";
         this.EntidadId = 0;
         this.NomResponsable = "";
-        this.Action = ProcessActionEnum.Add;
         this.DetalleItems = [];
 
     }
@@ -84,11 +65,11 @@ export class DespachoDetalleModel {
     CantidadSolicitado: number;
     CantidadReservado: number;
     CantidadAtendido: number;
-    Action: ProcessActionEnum
+    Action: number
     DespachoDetalleId: number;
     DespachoId: number;
     Cantidad: number;
-    DetalleReserva: Reserva[]
+    DetalleReserva: DespachoReservaOPModel[]
 
     constructor() {
         this.OrdenPedidoId = 0;
@@ -98,7 +79,7 @@ export class DespachoDetalleModel {
         this.CantidadSolicitado = 0;
         this.CantidadReservado = 0;
         this.CantidadAtendido = 0;
-        this.Action = ProcessActionEnum.Add;
+        this.Action = 0;
         this.DespachoDetalleId = 0;
         this.DespachoId = 0;
         this.Cantidad = 0;
@@ -112,7 +93,7 @@ export class DespachoReservaOPModel {
     MercaderiaId: number;
     Cantidad: number;
     StockId: number;
-    Action: ProcessActionEnum
+
     constructor() {
 
         this.ReservaId = 0;
@@ -121,6 +102,5 @@ export class DespachoReservaOPModel {
         this.MercaderiaId = 0;
         this.Cantidad = 0;
         this.StockId = 0;
-        this.Action = ProcessActionEnum.Add;
     }
 }
