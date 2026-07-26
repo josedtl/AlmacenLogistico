126cd8320b5919bd38c4a7efeb5b94a3a8d4cd8f
josedtl <30784219+josedtl@users.noreply.github.com>
2026-07-26 15:43:43 -0500

    Version 2
    add

--- Proyecto/app2/api.tsx
+++
@@ -1,5 +0,0 @@
-export const GetCategoria = async () => {
-    const res = await fetch('http://192.168.18.19:9085/api/Categoria/GetItems/')
-    const data = await res.json()
-    return data.Value
-}
